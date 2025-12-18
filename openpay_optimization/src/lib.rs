c4507135d0cbc3d7d3d5f8a349acefbb40179886
deji <poche450@gmail.com>
2025-12-17 21:55:56 -0500

    Refactor: Remove openpay_optimization crate (moved to separate repo)

--- openpay_optimization/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod engine;
-pub mod merit_matrix;
-#[cfg(feature = "pay_equity")]
-pub mod pay_equity;
-pub mod wage_scale;
-
-pub use engine::{OptimizationEngine, OptimizationResult, ProblemDefinition};
